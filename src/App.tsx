612ebf81d33b122fc50caf9b7abc589d1c76d3e6
karan-airmeet <karan@airmeet.com>
2024-04-25 21:56:19 +0530

    create react app, added input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const onSearch = useCallback((e: { target: { value: string } }) => {
+    setSearch(e.target.value);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input id="auto_complete_input" value={search} onChange={onSearch} />
+      <div></div>
     </div>
   );
 }
