0bf4626954852818031dd5ea659ead67fb5ad3be
karan-airmeet <karan@airmeet.com>
2024-04-25 22:04:30 +0530

    integrated countries API with search by country name

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
+import { CallApi } from "./Utils/CallApi";
 
 function App() {
   const [search, setSearch] = useState("");
+  const [countries, setCountries] = useState<string[]>([]);
   const onSearch = useCallback((e: { target: { value: string } }) => {
+    CallApi(`https://restcountries.com/v3.1/name/${e.target.value}`).then(
+      (res) => {
+        const countriesName: string[] = [];
+        console.log(res);
+        res.forEach((country: { name: { official: string } }) => {
+          countriesName.push(country.name.official);
+        });
+        setCountries(countriesName);
+      }
+    );
     setSearch(e.target.value);
   }, []);
+  useEffect(() => {
+    console.log(countries);
+  }, [countries]);
 
   return (
     <div className="App">
       <input id="auto_complete_input" value={search} onChange={onSearch} />
-      <div></div>
+      <div id="suggestions"></div>
     </div>
   );
 }
