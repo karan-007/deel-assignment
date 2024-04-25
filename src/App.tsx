42bff43b0a3e988e3316d324ef7697195d81265c
karan-airmeet <karan@airmeet.com>
2024-04-25 22:29:48 +0530

    Added suggestion box

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,18 @@ function App() {
   const [search, setSearch] = useState("");
   const [countries, setCountries] = useState<string[]>([]);
   const onSearch = useCallback((e: { target: { value: string } }) => {
-    CallApi(`https://restcountries.com/v3.1/name/${e.target.value}`).then(
-      (res) => {
+    CallApi(`https://restcountries.com/v3.1/name/${e.target.value}`)
+      .then((res) => {
         const countriesName: string[] = [];
-        console.log(res);
+        console.log(res.isError);
         res.forEach((country: { name: { official: string } }) => {
           countriesName.push(country.name.official);
         });
         setCountries(countriesName);
-      }
-    );
+      })
+      .catch(() => {
+        setCountries([]);
+      });
     setSearch(e.target.value);
   }, []);
   useEffect(() => {
@@ -24,8 +26,16 @@ function App() {
 
   return (
     <div className="App">
-      <input id="auto_complete_input" value={search} onChange={onSearch} />
-      <div id="suggestions"></div>
+      <div id="container">
+        <input id="auto_complete_input" value={search} onChange={onSearch} />
+        {!!countries.length && (
+          <div id="suggestions">
+            {countries.map((country) => {
+              return <span>{country}</span>;
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
